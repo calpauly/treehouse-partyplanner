d4d947e51274b0aa9ec6241c2fb7d263d9adae18
Paul Deffenbaugh <paul@brassringit.com>
2013-10-08 00:10:23 -0700

    Removed a comment just to test this out.

--- Party Planner/BRITMasterViewController.m
+++ Party Planner/BRITMasterViewController.m
@@ -28,7 +28,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
